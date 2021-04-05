3ffcfe438d2523c6bf521a5ec53d91643297131d
vanct <vanct10a11999@gmail.com>
2021-04-05 11:18:02 +0700

    Thiết kế menu cho ứng dụng

---
+++ Menu.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("MENU");
+        System.out.println("1.Draw the triangle");
+        System.out.println("2.Draw the square");
+        System.out.println("3.Draw the rectangle");
+        System.out.println("Exit");
+        System.out.println("Enter your choice:");
+        int a = sc.nextInt();
+
+        switch (a) {
+            case 1:
+                System.out.println("Draw the triangle");
+                System.out.println("******");
+                System.out.println("*****");
+                System.out.println("****");
+                System.out.println("***");
+                System.out.println("**");
+                System.out.println("*");
+                break;
+            case 2:
+                System.out.println("Draw the square");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                break;
+            case 3:
+                System.out.println("Draw the rectangle");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                break;
+            case 0:
+                System.exit(0);
+            default:
+                System.out.println("No choice!");
+
+        }
+    }
+}
