fee450aedf66eab1e5d64477e4376353b5b0a3e0
vanct <vanct10a11999@gmail.com>
2021-04-20 15:01:02 +0700

    StackList

---
+++ StackList/MyGenericStack.java
@@ -0,0 +1,24 @@
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+public class MyGenericStack<T> {
+    private LinkedList<T> stack;
+
+    public MyGenericStack() {
+        stack = new LinkedList<>();
+    }
+    public void push (T element) {
+        stack.addFirst(element);
+    }
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.removeFirst();
+    }
+    public int size() {
+        return stack.size();
+    }
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+}
