6528f3644bf4bd89952715582e45f1157527f9cf
vanct <vanct10a11999@gmail.com>
2021-04-29 08:25:48 +0700

    Array Stack

---
+++ Array Stack/MyStack.java
@@ -0,0 +1,31 @@
+public class MyStack {
+    private int arr[];
+    private int size;
+    private int index = 0;
+
+    public MyStack (int size) {
+        this.size = size;
+        arr = new int[size];
+    }
+    public void push (int element) {
+        if (isFull()) {
+            throw new StackOverflowError("Stack is full");
+        }
+        arr[index] = element;
+        index++;
+    }
+    public int pop() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Stack is null");
+        }
+        return arr[--index];
+    }
+    public int size() {
+        return index;
+    }
+    public boolean isEmpty() {
+        return index == 0;
+    }
+    public boolean isFull() {
+        return index == size;
+    }
